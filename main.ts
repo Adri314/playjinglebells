e40a05b391d77020c0ee389bed29b6efc69a5758
Adri314 <34138145+Adri314@users.noreply.github.com>
2022-12-14 11:50:25 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,84 @@
+function jingleAllTheWay () {
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.playTone(330, music.beat(BeatFraction.Breve))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+}
+function oneHorseOpenSleighEnd () {
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.playTone(349, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.playTone(262, music.beat(BeatFraction.Breve))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+}
+function sleighOh () {
+    music.playTone(294, music.beat(BeatFraction.Double))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.playTone(392, music.beat(BeatFraction.Double))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+}
+function playJingleBells () {
+    jingleBells()
+    jingleBells()
+    jingleAllTheWay()
+    ohWhatFunIt()
+    isToRideInA()
+    oneHorseOpen()
+    sleighOh()
+    jingleBells()
+    jingleBells()
+    jingleAllTheWay()
+    ohWhatFunIt()
+    isToRideInA()
+    oneHorseOpenSleighEnd()
+}
+function isToRideInA () {
+    music.playTone(349, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+}
+function jingleBells () {
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.playTone(330, music.beat(BeatFraction.Double))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+}
+function oneHorseOpen () {
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+}
+function ohWhatFunIt () {
+    music.playTone(349, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.playTone(349, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.playTone(349, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.playTone(349, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+}
+playJingleBells()
